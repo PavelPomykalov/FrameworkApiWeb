98ee9a12a42d9f7bfcf7749c2b9d877218cdfcd0
PavelPomykalov <102617293+PavelPomykalov@users.noreply.github.com>
2025-10-26 13:36:26 +0500

    Добавил библиотеку Faker для генерации данных

--- src/test/java/testdata/TestData.java
+++ src/test/java/testdata/TestData.java
@@ -1,15 +1,20 @@
 package testdata;
 
+import com.github.javafaker.Faker;
 import models.User;
 
 public class TestData {
+    public static Faker faker =  new Faker();
+
     public static final User DEFAULT_USER = User.builder()
-            .username("username")
-            .firstName("firstName")
-            .lastName("lastName")
-            .email("email")
-            .phone("password")
-            .userStatus(0)
+            .id(faker.number().numberBetween(1,99999))
+            .username(faker.name().name())
+            .firstName(faker.name().firstName())
+            .lastName(faker.name().lastName())
+            .password(faker.internet().password())
+            .email(faker.internet().emailAddress())
+            .phone(faker.numerify("2342342342"))
+            .userStatus(faker.number().numberBetween(1,3))
             .build();
 
     public static final User INVALID_USER = User.builder()
